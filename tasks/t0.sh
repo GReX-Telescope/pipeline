8d88f588cd03bb98887546e1fc199a41c3c57aa4
Kiran Shila <me@kiranshila.com>
2023-08-16 14:13:38 -0700

    update t0 task

--- tasks/t0.sh
+++ tasks/t0.sh
@@ -7,13 +7,12 @@ VOLTAGE_PATH="/hdd/data/voltages/"
 FILTERBANK_PATH="/hdd/data/filterbanks/"
 
 RUST_LOG=info ../t0/target/release/grex_t0 \
-        --requant-gain 32 \
         -d ${AVG_POW} \
         --vbuf-power ${VBUF_POW} \
         --trig \
         --fpga-addr ${FPGA_ADDR}:69 \
         --injection-cadence ${INJECT_S} \
         --dump-path ${VOLTAGE_PATH} \
-        --filterbank-path ${FILTERBANK_PATH} \
+        --filterbank-path ${FILTERBANK_PATH} # \
 #        filterbank
 #       psrdada -k ${KEY} -s ${DADA_SAMPLES}
